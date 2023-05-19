8aafc12de93e23d41faa5907b9215a5e04a15802
Marcos <marcos.moliveira@outlook.com>
2023-05-19 11:48:50 -0300

    oauth

---
+++ src/Components/Copyright.tsx
@@ -0,0 +1,16 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200 ">
+      Feito com 💜 no NLW por{' '}
+      <a
+        className="underline hover:text-gray-100"
+        href="https://github.com/MarcosMOliveiradev"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {' '}
+        Marcos
+      </a>
+    </div>
+  )
+}
