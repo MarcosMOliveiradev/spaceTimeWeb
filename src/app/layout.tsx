fd9d9f4b80b00488d0f1f5bf5c002c2eb45b73af
Marcos <marcos.moliveira@outlook.com>
2023-05-21 19:43:47 -0300

    finalização das aulas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
+import { Copyright } from '@/Components/Copyright'
+import { Hero } from '@/Components/Hero'
+import { Signin } from '@/Components/Signin'
+import { Profile } from '@/Components/profile'
 import {
   Bai_Jamjuree as BaiJanjuree,
   Roboto_Flex as Roboto,
 } from 'next/font/google'
+import { cookies } from 'next/headers'
 import React from 'react'
 import './globals.css'
 
@@ -23,12 +28,31 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const isAuthenticated = cookies().has('token')
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJanjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2 bg-[url(../assets/bg-stars.svg)] bg-cover">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full " />
+
+            {/* Stripers */}
+            <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes" />
+
+            {isAuthenticated ? <Profile /> : <Signin />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex max-h-screen flex-col overflow-y-scroll bg-[url(../assets/bg-stars.svg)] bg-cover">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
